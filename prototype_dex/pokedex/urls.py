e4e58dac9cd9ad264d9e6e3971fdd9184c0cc75e
Jellymancer <jedrzejjawor@gmail.com>
2022-07-17 23:21:10 +0200

    Tilføjeg Pokedex appen til prototype dex. Appen indeholder en database med pokemonner. Udviklet en UI til at tilføje/fjerne/redigere pokemonner direkte fra browser.

--- prototype_dex/pokedex/urls.py
+++ prototype_dex/pokedex/urls.py
@@ -11,4 +11,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord,
+         name='updaterecord'),
 ]
--- prototype_dex/pokedex/views.py
+++ prototype_dex/pokedex/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from .models import Pokedex
 
@@ -25,3 +26,47 @@ def index(request):
         'mypokedex': mypokedex,
         }
     return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    name = request.POST['name']
+    primarytype = request.POST['primarytype']
+    secondarytype = request.POST['secondarytype']
+
+    dex_member = Pokedex(name=name, primarytype=primarytype,
+                         secondarytype=secondarytype)
+    dex_member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    dex_member = Pokedex.objects.get(id=id)
+    dex_member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def update(request, id):
+    dex_member = Pokedex.objects.get(id=id)
+    template = loader.get_template("update.html")
+    context = {
+        "dex_member": dex_member
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    name = request.POST['name']
+    primarytype = request.POST['primarytype']
+    secondarytype = request.POST['secondarytype']
+
+    dex_member = Pokedex.objects.get(id=id)
+    dex_member.name = name
+    dex_member.primarytype = primarytype
+    dex_member.secondarytype = secondarytype
+    dex_member.save()
+    return HttpResponseRedirect(reverse('index'))
