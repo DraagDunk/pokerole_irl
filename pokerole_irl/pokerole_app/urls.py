3d412ec9ff6f2f680f2fd280dd64b2f8a508cdfc
Jellymancer <jedrzejjawor@gmail.com>
2022-09-14 19:13:55 +0200

    Fixed urls reference to RegisterView

--- pokerole_irl/pokerole_app/urls.py
+++ pokerole_irl/pokerole_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MainPageView.as_view(), name="index"),
-    path('register', views.Register.as_view(), name='register')
+    path('register', views.RegisterView.as_view(), name='register')
 ]
