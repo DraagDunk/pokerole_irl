000c74cf8425f117945604c90be779387c152410
Jellymancer <jedrzejjawor@gmail.com>
2022-10-08 18:01:54 +0200

    corrected a reference  in pokerole_app/urls.py

--- pokerole_irl/pokerole_app/urls.py
+++ pokerole_irl/pokerole_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import AllSpeciesView, MainPageView, RegisterView, profile, TestView
+from .views import AllSpeciesView, MainPageView, RegisterView, UserProfileView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name="index"),
     path('all_species/', AllSpeciesView.as_view(), name="all_species"),
     path('register', RegisterView.as_view(), name='register'),
-    path('accounts/profile/', TestView.as_view(), name='user-profile')
+    path('accounts/profile/', UserProfileView.as_view(), name='user-profile')
 ]
