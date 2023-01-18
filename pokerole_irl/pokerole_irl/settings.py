798558c6a84787cb766897b405f696844a6daa55
Jesper Dam <jesper.dk@hotmail.dk>
2023-01-18 21:26:23 +0100

    Remove docker, apply pipenv

--- pokerole_irl/pokerole_irl/settings.py
+++ pokerole_irl/pokerole_irl/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pokerole_app',
+    'health_check'
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
 
 # Default primary key field type
