c8fa4c41b9266c0c5c605f1016b95a50cc7600c6
Jedrzej <jedrzejjawor@gmail.com>
2023-02-23 19:54:26 +0100

    Added links and improved templates

--- pokerole_irl/user_app/urls.py
+++ pokerole_irl/user_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('setting_list/', SettingListView.as_view(), name='setting-list'),
     path('setting_create/', SettingCreateView.as_view(), name='setting-create'),
-    path('setting_<pk>/', SettingView.as_view(), name='setting')
+    path('setting/<int:pk>', SettingView.as_view(), name='setting')
 ]
