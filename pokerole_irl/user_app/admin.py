96f6a8c0eac19443bad559b40724095b61008267
Jedrzej <jedrzejjawor@gmail.com>
2023-02-24 20:10:52 +0100

    basic character view and templates

--- pokerole_irl/user_app/admin.py
+++ pokerole_irl/user_app/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Profile, Setting
+from .models import Profile, Setting, Character
 
 admin.site.register(Profile)
 
 @admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
--- pokerole_irl/user_app/urls.py
+++ pokerole_irl/user_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, UserProfileView, SettingListView, SettingCreateView, SettingView
+from .views import RegisterView, UserProfileView, SettingListView, SettingCreateView, SettingView, CharacterView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('setting_list/', SettingListView.as_view(), name='setting-list'),
     path('setting_create/', SettingCreateView.as_view(), name='setting-create'),
-    path('setting/<int:pk>', SettingView.as_view(), name='setting')
+    path('setting/<int:pk>', SettingView.as_view(), name='setting'),
+    path('character/<int:pk>', CharacterView.as_view(), name='character')
 ]
