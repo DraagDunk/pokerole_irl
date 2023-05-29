40971bf72cdfb3cac591580c906e09eb2132f722
Jesper Dam <jesper.dk@hotmail.dk>
2023-05-29 23:27:47 +0200

    Calculate damage and accuracy on move

--- pokerole_irl/pokerole_app/templatetags/utilities.py
+++ pokerole_irl/pokerole_app/templatetags/utilities.py
@@ -9,3 +9,12 @@ def stat_graphic(base_stat, max_stat):
     stat_str += "🔴"*base_stat
     stat_str += "⚪"*(max_stat-base_stat)
     return stat_str
+
+
+@register.simple_tag
+def calc_accuracy(move, pokemon):
+    return move.calc_accuracy(pokemon)
+
+@register.simple_tag
+def calc_damage(move, pokemon):
+    return move.calc_damage(pokemon)
