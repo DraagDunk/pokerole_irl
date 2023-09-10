7acafadf21d9e6c9cb837b71e5744e0ef7f4e2de
Jesper Dam <jesper.dk@hotmail.dk>
2023-09-10 21:41:35 +0200

    Profile detail and update + 403

--- pokerole_irl/user_app/models.py
+++ pokerole_irl/user_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
+from django.urls import reverse_lazy
 
 
 class Profile(models.Model):
@@ -13,6 +14,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_absolute_url(self):
+        return reverse_lazy("user-profile", kwargs={"pk": self.pk})
 
     def save(self, **kwargs):
         self.slug = slugify(self.user.username)
